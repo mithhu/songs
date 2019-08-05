95392345de7d5eeb97c3ba3203bf8485e4d37d0e
mithhu <mithhu666@gmail.com>
2019-08-06 01:39:53 +0600

    stephen

---
+++ src/reducers/index.js
@@ -0,0 +1,22 @@
+import { combineReducers } from "redux";
+
+const songReducer = () => {
+  return [
+    { title: "No Scrubs", duration: "4:05" },
+    { title: "Macarena", duration: "2:30" },
+    { title: "All star", duration: "3:15" },
+    { title: "I want it that way", duration: "1:45" }
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECT") {
+    return action.payload;
+  }
+  return selectedSong;
+};
+
+export default combineReducers({
+  songs: songReducer,
+  selectedSong: selectedSongReducer
+});
